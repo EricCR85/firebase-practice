61f07d398e968460dbc9b62616ece25fb696d755
EricCR85 <ecr.ceo@gmail.com>
2026-01-29 01:03:05 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,22 +1,84 @@
-import './App.css';
-import { auth } from './firebase/init';
-import { createUserWithEmailAndPassword } from "firebase/auth";
-
+import React from "react";
+import "./App.css";
+import { auth, db } from "./firebase/init";
+import { addDoc, collection, getDocs, getDoc, doc } from "firebase/firestore";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
+} from "firebase/auth";
 
 function App() {
-  function register() {
-    console.log('register')
-    createUserWithEmailAndPassword(auth, 'email@email.com', 'test12')
-    .then((user) => {
-      console.log(user)
-    })
-    .catch((error) => {
-      console.log(error)
-    })
+  const [user, setUser] = React.useState({});
+  const [loading, setLoading] = React.useState(true);
+
+  function createPost() {
+    const post = {
+      title: "Land a 100k job",
+      description: "Finish frontend Simplified",
+    };
+    addDoc(collection(db, "posts"), post);
   }
+
+  async function getAllPosts() {
+    const { docs } = await getDocs(collection(db, "posts"));
+    const posts = docs.map((elem) => ({ ...elem.data(), id: elem.id }));
+    console.log(posts);
+  }
+
+  async function getPostById() {
+    const hardcodedId = "3U3wC4V5KKfykUtHevNG";
+    const postRef = doc(db, "posts", hardcodedId);
+    const postSnap = await getDoc(postRef);
+    const post = postSnap.data();  
+    console.log(post);
+  }
+
+  React.useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      setLoading(false);
+      console.log(user);
+      if (user) {
+        setUser(user);
+      }
+    });
+  }, []);
+
+  function register() {
+    console.log("register");
+    createUserWithEmailAndPassword(auth, "email@email.com", "test123")
+      .then((user) => {
+        console.log(user);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  function login() {
+    signInWithEmailAndPassword(auth, "email@email.com", "test123").then(
+      ({ user }) => {
+        console.log(user);
+        setUser(user);
+      },
+    );
+  }
+
+  function logout() {
+    signOut(auth);
+    setUser({});
+  }
+
   return (
     <div className="App">
-   <button onClick={register}>Register</button>
+      <button onClick={register}>Register</button>
+      <button onClick={login}>Login</button>
+      <button onClick={logout}>Logout</button>
+      {loading ? "loading..." : user.email}
+      <button onClick={createPost}>Create Post</button>
+      <button onClick={getAllPosts}>Get All Post</button>
+      <button onClick={getPostById}>Get post by Id</button>
     </div>
   );
 }
